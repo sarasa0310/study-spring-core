239d9e6decba3a746f4d8e91c85d59953bd0014e
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-07-23 20:22:31 +0900

    싱글톤 및 프로토타입 빈 스코프 학습
    - 싱글톤 스코프 빈 : 스프링 컨테이너의 시작부터 종료까지 생명주기를 같이 함
    - 프로토타입 스코프 빈 : 빈 생성 및 의존관계 주입 후 더 이상 스프링 컨테이너에서 관리 X
    - 싱글톤과 프로토타입을 혼용할 시 주의 필요. 싱글톤 빈에서 매번 새로운 프로토타입 빈 의존성 주입이 필요하다면 ObjectProvider를 활용할 수 있다.

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest.java
@@ -0,0 +1,85 @@
+package hello.core.scope;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonWithPrototypeTest {
+
+    @Test
+    void prototypeFindTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean2.addCount();
+        assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+
+    @Test
+    void clientBeanWithPrototypeTest() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+
+        assertThat(count1).isEqualTo(1);
+        assertThat(count2).isEqualTo(1);
+    }
+
+    @Scope("singleton")
+    @RequiredArgsConstructor
+    static class ClientBean {
+
+//        private final PrototypeBean prototypeBean;
+
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+
+    }
+
+}
